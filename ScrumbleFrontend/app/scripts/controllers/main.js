98d9170a0c234d392e15f492117cf86964b55565
lukaw3d <lukaw3d@gmail.com>
2014-03-22 01:01:45 +0100

    yo angular

---
+++ ScrumbleFrontend/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('scrumbleFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
