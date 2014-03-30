6d571c0dde22238b4db6f62041be753e88034afb
Andraz Bajt <edofic@gmail.com>
2014-03-30 12:42:14 +0200

    user authentication and password updating.
    no authorization in place!

---
+++ Scrumble/Handler/Authentication.hs
@@ -0,0 +1,25 @@
+module Handler.Authentication (postAuthenticationR) where
+
+import Import
+import Yesod.Auth 
+import GHC.Generics (Generic)
+import Crypto.PasswordStore (verifyPassword)
+import Data.Text.Encoding (encodeUtf8)
+import Network.HTTP.Types.Status (unauthorized401)
+
+data AuthRequest = AuthRequest { username :: Text
+                               , password :: Text     
+                               } deriving Generic
+instance FromJSON AuthRequest
+
+postAuthenticationR :: Handler ()
+postAuthenticationR = do
+  AuthRequest username givenPassword <- requireJsonBody
+  mpass <- runDB $ getBy $ UniqueUserAuth username
+  flip (maybe notFound) mpass $ \entity -> 
+    let realPass = userAuthPassword $ entityVal entity
+        passOk = verifyPassword (encodeUtf8 givenPassword) (encodeUtf8 realPass)
+    in  if passOk then 
+          setCreds False $ Creds "jsonPost" username []
+        else 
+          sendResponseStatus unauthorized401 ()
---
+++ Scrumble/Handler/UsersUserPassword.hs
@@ -0,0 +1,27 @@
+module Handler.UsersUserPassword (postUsersUserPasswordR) where
+
+import Import
+import Crypto.PasswordStore (makePassword)
+import Control.Monad.Maybe
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
+import GHC.Generics (Generic)
+
+data PasswordRequest = PasswordRequest { newPassword :: Text }
+                                       deriving Generic
+
+instance FromJSON PasswordRequest
+
+postUsersUserPasswordR :: UserId -> Handler ()
+postUsersUserPasswordR userId = do
+  PasswordRequest newPassword <- requireJsonBody
+  hashed <- liftIO $ decodeUtf8 `fmap` makePassword (encodeUtf8 newPassword) 14
+  ok <- runDB $ runMaybeT $ do
+    user <- MaybeT $ get userId
+    let username = userUsername user
+        newAuth = UserAuth username hashed
+    curAuth <- lift $ getBy $ UniqueUserAuth username
+    lift $ maybe (insert_ newAuth) 
+                 (\(Entity key _) -> replace key newAuth) 
+                 curAuth
+  maybe notFound return ok
+
