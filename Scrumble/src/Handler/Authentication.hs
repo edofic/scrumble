14fd1066cc9254a855d4e14161175b3ce788a42c
Andraz Bajt <edofic@gmail.com>
2014-04-07 21:14:11 +0200

    api: login failure json msgs

--- Scrumble/src/Handler/Authentication.hs
+++ Scrumble/src/Handler/Authentication.hs
@@ -6,7 +6,7 @@ import GHC.Generics (Generic)
 import Crypto.PasswordStore (verifyPassword)
 import Data.Text.Encoding (encodeUtf8)
 import Authorization
-import Network.HTTP.Types.Status (unauthorized401)
+import Network.HTTP.Types.Status (unauthorized401, notFound404)
 
 data AuthRequest = AuthRequest { username :: Text
                                , password :: Text     
@@ -17,11 +17,15 @@ postAuthenticationR :: Handler ()
 postAuthenticationR = do
   AuthRequest username givenPassword <- requireJsonBody
   mpass <- runDB $ getBy $ UniqueUserAuth username
-  flip (maybe notFound) mpass $ \entity -> 
+  flip (maybe notFoundMsg) mpass $ \entity -> 
     let realPass = userAuthPassword $ entityVal entity
         passOk = verifyPassword (encodeUtf8 givenPassword) (encodeUtf8 realPass)
     in  if passOk then 
           setCreds False $ Creds "jsonPost" username []
         else 
-          sendResponseStatus unauthorized401 ("Wrong password" :: Text)
-          
+          unauthorizedMsg
+  where
+    unauthorizedMsg = sendResponseStatus unauthorized401 $ object 
+      [("message" :: Text, toJSON ("Login failed. Wrong password." :: Text))]
+    notFoundMsg = sendResponseStatus notFound404 $ object
+      [("message" :: Text, toJSON ("Login failed. User not found." :: Text))]
