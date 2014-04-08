6ba2e631167f62d6a151817048f0b6733d752e75
Blaz Repas <info@bss.si>
2014-04-08 11:32:55 +0200

    API: User story validation

--- Scrumble/src/Handler/StoriesStory.hs
+++ Scrumble/src/Handler/StoriesStory.hs
@@ -1,9 +1,14 @@
-module Handler.StoriesStory (getStoriesStoryR, putStoriesStoryR, deleteStoriesStoryR) where
+module Handler.StoriesStory 
+(getStoriesStoryR
+,putStoriesStoryR
+,deleteStoriesStoryR
+,userStoryValidations) where
 
 import Import  hiding ((.||.))
 import qualified Authorization as Auth
 import Authorization ((.||.))
 import Model.ProjectRole
+import Validation
 
 getStoriesStoryR :: ProjectId -> StoryId -> Handler Value
 getStoriesStoryR projectId storyId = do
@@ -12,14 +17,18 @@ getStoriesStoryR projectId storyId = do
   story <- maybe notFound return storyMby
   return $ (toJSON . FlatEntity) story
 
-
 assertionOwnerMaster :: ProjectId -> (Entity User -> Handler Bool)
 assertionOwnerMaster projectId = (Auth.roleOnProject ProductOwner projectId) .||. (Auth.roleOnProject ScrumMaster projectId)
+
+userStoryValidations story = do
+  ("businessValue", "Business value should be non-negative") `validate`
+    (storyBusinessValue story >= 0)
 
 putStoriesStoryR :: ProjectId -> StoryId -> Handler ()
 putStoriesStoryR projectId storyId = do
   Auth.assertM $ assertionOwnerMaster projectId
   story <- requireJsonBodyWith [("project", toJSON projectId)]
+  runValidationHandler $ userStoryValidations story
   runDB $ replace storyId story
 
 deleteStoriesStoryR :: ProjectId -> StoryId -> Handler ()
