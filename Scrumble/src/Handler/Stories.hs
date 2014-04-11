ca36010fc4c325afb5ac7d3fc2a2ac1aaef3249f
Blaz Repas <info@bss.si>
2014-04-11 21:57:26 +0200

    Story name uniqueness validations

--- Scrumble/src/Handler/Stories.hs
+++ Scrumble/src/Handler/Stories.hs
@@ -4,6 +4,7 @@ import Import
 import qualified Authorization as Auth
 import Validation
 import Handler.StoriesStory (userStoryValidations, assertionOwnerMaster)
+import Data.Maybe (isJust)
 
 getStoriesR :: ProjectId -> Handler Value
 getStoriesR projectId = do
@@ -11,10 +12,13 @@ getStoriesR projectId = do
   stories :: [Entity Story] <- runDB $ selectList [StoryProject ==. projectId] []
   return $ array $ (toJSON . FlatEntity) `fmap` stories
 
-postStoriesR :: ProjectId -> Handler String
+postStoriesR :: ProjectId -> Handler ()
 postStoriesR projectId = do
   Auth.assertM $ assertionOwnerMaster projectId
   story :: Story <- requireJsonBodyWith [("project", toJSON projectId)]
   runValidationHandler $ userStoryValidations story
-  storyId <- runDB $ insert story
-  return $ show storyId
+  storyIdMby <- runDB $ insertUnique story
+  runValidationHandler $ do
+    ("title", "Story with supplied title already exists") `validate`
+      (isJust storyIdMby)
+  return ()
